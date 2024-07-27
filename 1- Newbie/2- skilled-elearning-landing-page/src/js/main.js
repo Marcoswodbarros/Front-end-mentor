1e527717b80dfeb30f72c29985de33e9578255c4
Marcos Barros <marcoswodbarros1@gmail.com>
2024-07-27 14:54:33 -0300

    Finishing NB2 Intro section - Tablet

---
+++ 1- Newbie/2- skilled-elearning-landing-page/src/js/main.js
@@ -0,0 +1,19 @@
+const logo = document.querySelector('.header-logo');
+const woman = document.querySelector('.intro-img');
+
+function changePics () {
+    if (window.innerWidth < 768) {
+        logo.src = '/src/assets/images/header/logo.png';
+        woman.src = '/src/assets/images/intro/mobile-woman.png';
+        
+    } else if (window.innerWidth >= 768 && window.innerWidth < 1440) {
+        logo.src = '/src/assets/images/header/logo.png';
+        woman.src = '/src/assets/images/intro/tablet-woman.png';
+
+    } else {
+        logo.src = '/src/assets/images/header/desktop-logo.png';
+    }
+}
+
+changePics();
+window.addEventListener('resize', changePics);
