7f45dee899f79b15aac82751754adec959c0bf1a
Marcos Barros <marcoswodbarros1@gmail.com>
2024-08-17 15:28:02 -0300

    Starting NB2 footer - Desktop

--- 1- Newbie/2- skilled-elearning-landing-page/src/js/main.js
+++ 1- Newbie/2- skilled-elearning-landing-page/src/js/main.js
@@ -1,18 +1,22 @@
 const logo = document.querySelector('.header-logo');
 const woman = document.querySelector('.intro-img');
+const logoFooter = document.querySelector('.footer-container img');
 
 function changePics () {
     if (window.innerWidth < 768) {
         logo.src = '/src/assets/images/header/logo.png';
         woman.src = '/src/assets/images/intro/mobile-woman.png';
+        logoFooter.src = '/src/assets/images/footer/logo-skilled.png';
         
     } else if (window.innerWidth >= 768 && window.innerWidth < 1440) {
         logo.src = '/src/assets/images/header/logo.png';
         woman.src = '/src/assets/images/intro/tablet-woman.png';
+        logoFooter.src = '/src/assets/images/footer/logo-skilled.png';
 
     } else {
         logo.src = '/src/assets/images/header/desktop-logo.png';
         woman.src = '/src/assets/images/intro/desktop-woman.png';
+        logoFooter.src = '/src/assets/images/footer/logo-skilled-desktop.png';
     }
 }
 
