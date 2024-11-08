d0932cfdd3f3114c35403b0b3aa0d4a22b2899b5
Marcos Barros <marcoswodbarros1@gmail.com>
2024-11-08 15:38:04 -0300

    Finishing NB4 Intro section 'desktop version'

--- 1- Newbie/3- equalizer-landing-page/src/js/main.js
+++ 1- Newbie/3- equalizer-landing-page/src/js/main.js
@@ -1,4 +1,5 @@
 let introBg = document.querySelector('.intro-bg');
+let introBg2 = document.querySelector('.intro-bg2');
 
 let priceBg = document.querySelector('.price-bg');
 let priceCellphone = document.querySelector('.price-cellphone');
@@ -7,17 +8,22 @@ function resizePics() {
 
     if (window.innerWidth < 768) {
         introBg.src = 'assets/images/1_Intro/bg-mobile.png';
+        // Não possui introBg2
 
         priceBg.src = 'assets/images/2_Price/bg-mobile.png';
         priceCellphone.src = 'assets/images/2_Price/cellphone-mobile.png';
 
     } else if (window.innerWidth >= 768 && window.innerWidth < 1440) {
         introBg.src = 'assets/images/1_Intro/bg-tablet.png';
+        introBg2.src = 'assets/images/1_Intro/bg2-tablet.png';
 
         priceBg.src = 'assets/images/2_Price/bg-tablet.svg';
         priceCellphone.src = 'assets/images/2_Price/cellphone-tablet.png';
 
     } else {
+        // Utiliza o mesmo introBg do tablet
+        introBg2.src = 'assets/images/1_Intro/bg2-desktop.png';
+
         priceBg.src = 'assets/images/2_Price/bg2-desktop.png';
         priceCellphone.src = 'assets/images/2_Price/cellphone-desktop.png';
     }
